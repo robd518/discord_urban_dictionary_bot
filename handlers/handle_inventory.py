fe425c47e5caa777583ed2fee866533122805e15
Rob D'Aveta <rob.daveta@gmail.com>
2020-05-15 12:51:24 -0400

    refactored how lootboxes are found to avoid unnecessary complexity

--- handlers/handle_inventory.py
+++ handlers/handle_inventory.py
@@ -13,11 +13,12 @@ async def handle_inventory(player_inventory):
     if not player_inventory:
         return inventory_actions
 
-    elif "life potion" not in player_inventory:
+    # look for non-existence of life potions specifically
+    if "life potion" not in player_inventory:
         inventory_actions.append((1, "buy life potion 50"))
 
-    elif next(k for k in player_inventory.keys() if k.endswith("lootbox")):
-        lootbox = next(k for k in player_inventory.keys() if k.endswith("lootbox"))
-        inventory_actions.append((2, f"open {lootbox}"))
+    for key in player_inventory.keys():
+        if key.endswith("lootbox"):
+            inventory_actions.append((2, f"open {lootbox}"))
 
     return inventory_actions
