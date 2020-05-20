38abb4b31b2389608bcf3989102704edd81e8eae
Rob D'Aveta <rob.daveta@gmail.com>
2020-05-20 14:55:27 -0400

    modified life check to use an environment variable instead of a value set inside the code

--- handlers/handle_life_check.py
+++ handlers/handle_life_check.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import re
 
-def handle_life_check(message):
+def handle_life_check(message, hp_threshold):
     ''' Read the discord message that would represent your character
         returning from battle, and determine if their life is below
-        a certain threshold
+        a certain threshold which is passed in the "hp_threshold" variable
 
         Returns:
             True: is below threshold
@@ -12,7 +12,7 @@ def handle_life_check(message):
     '''
     
     try:
-        if int(re.search('\d{1,3}\/\d{3}', message).group(0).split('/')[0]) <= 160:
+        if int(re.search('\d{1,3}\/\d{3}', message).group(0).split('/')[0]) <= hp_threshold:
             return True
         else:
             return False
