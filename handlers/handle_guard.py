cb1a3828e91a8ba22c323cc74eef2bff6f442f5e
Rob D'Aveta <rob.daveta@gmail.com>
2020-05-21 18:35:02 -0400

    updated captcha solver based on lots of testing

--- handlers/handle_guard.py
+++ handlers/handle_guard.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
+import requests
+from PIL import Image
+from io import BytesIO
 
-captchas = {
-    "https://cdn.discordapp.com/attachments/703333986042380411/709454247351025744/epic_guard.png": "wolf skin",
-    "https://cdn.discordapp.com/attachments/703333986042380411/709443647388516432/epic_guard.png": "apple",
-    "https://cdn.discordapp.com/attachments/703333986042380411/709441377666203658/epic_guard.png": "golden fish",
-    "https://cdn.discordapp.com/attachments/703333986042380411/709427399787282512/epic_guard.png": "banana",
-    "https://cdn.discordapp.com/attachments/703333986042380411/709256572777267250/epic_guard.png": "zombie eye",
-    "https://cdn.discordapp.com/attachments/703333986042380411/709522524261842964/epic_guard.png": "zombie eye",
-    "https://cdn.discordapp.com/attachments/703333986042380411/709231236471717908/epic_guard.png": "epic coin",
-    "https://cdn.discordapp.com/attachments/703333986042380411/709219030434709604/epic_guard.png": "normie fish",
-    "https://cdn.discordapp.com/attachments/703333986042380411/709053020180054016/epic_guard.png": "life potion",
-    "https://cdn.discordapp.com/attachments/703333986042380411/709031733504245840/epic_guard.png": "golden coin"
+matches = {
+    (237, 28, 36, 255) : "apple",
+    (253, 215, 0, 255) : "banana",
+    (47, 21, 62, 255) : "epic coin",
+    (255, 204, 0, 255) : "golden fish",
+    (255, 242, 0, 255) : "golden coin",
+    (217, 17, 27, 255) : "life potion",
+    (0, 198, 255, 255) : "normie fish",
+    (255, 21, 21, 255) : "ruby",
+    (255, 130, 170, 255) : "unicorn horn",
+    (46, 46, 52, 255) : "wolf skin",
+    (77, 98, 11, 255) : "zombie eye"
 }
 
 def handle_guard(message):
-    print(f"{message.guild} | {message.channel} | {message.author} | {message.content}")
-    print(message.attachments)
 
-    if message.attachments[0].url in captchas:
-        return captchas[message.attachments[0].url]
-
-    else:
-        return "not found"
+    r = requests.get(message.attachments[0].url)
+    img = Image.open(BytesIO(r.content))
+    pixel_data = set(img.getdata())
+    for k, v in matches.items():
+        if k in pixel_data:
+            return matches[k]
+    
+    return "none found"
